b040d3e82033edb0ea174ed2fbb0eb4ffebebe2e
Suki <tues1205@naver.com>
2025-12-01 05:04:22 +0900

    ✨ Feat: 채팅페이지로 이동 기능

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,11 +1,17 @@
 import s from "./Input.module.scss";
 import SEARCH from "@assets/icons/search_icon.svg";
 
-const Input = ({ width = "55.3125rem", height = "4.9375rem" }) => {
+const Input = ({ width = "55.3125rem", height = "4.9375rem", onClick }) => {
   return (
     <div className={s.InputContainer} style={{ width, height }}>
       <input placeholder="ex. 커플끼리 할 2만원 이하 게임 추천해줘" />
-      <img src={SEARCH} alt="검색" className={s.searchButton} />
+      <img
+        src={SEARCH}
+        alt="검색"
+        className={s.searchButton}
+        style={{ width: "1.6rem" }}
+        onClick={onClick}
+      />
     </div>
   );
 };
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -2,8 +2,11 @@ import s from "./MainPage.module.scss";
 import Logo from "../../components/logo/Logo";
 import Input from "../../components/input/Input";
 import useTypewriter from "../../utils/useTypewriter";
+import useNavigation from "../../hooks/useNavagation";
 
 const MainPage = () => {
+  const { goTo } = useNavigation();
+
   const fullText =
     "11만 개의 Steam 게임 중, 당신의 취향을 저격할 게임을 찾아드립니다";
   const typingText = useTypewriter(fullText, 100);
@@ -12,7 +15,7 @@ const MainPage = () => {
     <div className={s.MainPageContainer}>
       <Logo />
       <p className={s.descriptionText}>{typingText}</p>
-      <Input />
+      <Input onClick={() => goTo(`/chat`)} />
     </div>
   );
 };
