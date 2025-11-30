99cf876101ec39f704089d0107a7b3b3bad89170
Suki <tues1205@naver.com>
2025-12-01 04:20:23 +0900

    💄 Design: AI 답변 말풍선 컴포넌트

---
+++ src/pages/chatPage/components/aiBubble/AiBubble.jsx
@@ -0,0 +1,36 @@
+import { useMemo } from "react";
+import s from "./AiBubble.module.scss";
+import { parseAiResponse } from "../../../../utils/parseAiResponse";
+import AI from "@assets/images/ai_profile.png";
+
+const AiBubble = ({ text }) => {
+  const { intro, games } = useMemo(() => parseAiResponse(text), [text]);
+
+  return (
+    <div className={s.aiBubbleContainer}>
+      {/* 상단 멘트 */}
+      {intro && (
+        <div className={s.introSection}>
+          <img src={AI} alt="ai" style={{ width: "2.06rem" }} />{" "}
+          <div className={s.introBubble}>{intro}</div>
+        </div>
+      )}
+
+      {/* 게임 카드 리스트 */}
+      <div className={s.cardList}>
+        {games.map((game, index) => (
+          <div key={index} className={s.gameCard}>
+            <div className={s.iconPlaceholder}>🎮</div>
+
+            <div className={s.textInfo}>
+              <h4 className={s.gameTitle}>{game.title}</h4>
+              <p className={s.gameDesc}>{game.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AiBubble;
