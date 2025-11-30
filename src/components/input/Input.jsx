1d316221a3a42dd4ca2245bb22128779ca5400be
Suki <tues1205@naver.com>
2025-12-01 04:19:13 +0900

    💄 Design: Input 디자인 수정

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -4,10 +4,8 @@ import SEARCH from "@assets/icons/search_icon.svg";
 const Input = ({ width = "55.3125rem", height = "4.9375rem" }) => {
   return (
     <div className={s.InputContainer} style={{ width, height }}>
-      <span className={s.InputSpan}>
-        <input placeholder="ex. 커플끼리 할 2만원 이하 게임 추천해줘" />
-        <img src={SEARCH} alt="검색" className={s.searchButton} />
-      </span>
+      <input placeholder="ex. 커플끼리 할 2만원 이하 게임 추천해줘" />
+      <img src={SEARCH} alt="검색" className={s.searchButton} />
     </div>
   );
 };
