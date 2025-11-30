f243c6f66deba109c650c42f2b2d4e39ead53452
Suki <tues1205@naver.com>
2025-12-01 04:41:40 +0900

    💄 Design: 메인페이지 텍스트 애니메이션

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,14 +1,17 @@
 import s from "./MainPage.module.scss";
 import Logo from "../../components/logo/Logo";
 import Input from "../../components/input/Input";
+import useTypewriter from "../../utils/useTypewriter";
 
 const MainPage = () => {
+  const fullText =
+    "11만 개의 Steam 게임 중, 당신의 취향을 저격할 게임을 찾아드립니다";
+  const typingText = useTypewriter(fullText, 100);
+
   return (
     <div className={s.MainPageContainer}>
       <Logo />
-      <p className={s.descriptionText}>
-        11만 개의 Steam 게임 중, 당신의 취향을 저격할 게임을 찾아드립니다
-      </p>
+      <p className={s.descriptionText}>{typingText}</p>
       <Input />
     </div>
   );
