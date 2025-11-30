6d3a99bb4e20ab56dbd436dd8f5a6011b5a6d946
Suki <tues1205@naver.com>
2025-12-01 02:41:20 +0900

    💄 Design: 메인페이지 UI 구현

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import s from "./MainPage.module.scss";
+import Logo from "../../components/logo/Logo";
+import Input from "../../components/input/Input";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className={s.MainPageContainer}>
+      <Logo />
+      <p className={s.descriptionText}>
+        11만 개의 Steam 게임 중, 당신의 취향을 저격할 게임을 찾아드립니다
+      </p>
+      <Input />
+    </div>
+  );
 };
 
 export default MainPage;
