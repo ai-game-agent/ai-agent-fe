4fe987290a5993fc0352134de92564e3877e3544
Suki <tues1205@naver.com>
2025-12-01 01:04:17 +0900

    🔧 Chore: config 파일에 폴더 구조 정의

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@hooks": "/src/hooks",
+      "@apis": "/src/apis",
+      "@routes": "/src/routes",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+      "@types": "/src/types",
+      "@mocks": "/src/mocks",
+      "@assets": "/src/assets",
+      "@store": "/src/store",
+    },
+  },
+});
