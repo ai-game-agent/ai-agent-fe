fed454cc24044c7a24db8eb82b37a0c1ee09682b
Suki <tues1205@naver.com>
2025-12-01 01:04:36 +0900

    💄 Design: global 스타일 및 배경색 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <>hello world</>;
-}
+import "@styles/global.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./pages/mainPage/MainPage";
+
+const router = createBrowserRouter([{ path: "/", element: <MainPage /> }]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
