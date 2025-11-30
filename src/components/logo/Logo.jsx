e7bd8339c0d3853508fae80c33efb5a99fba4cbd
Suki <tues1205@naver.com>
2025-12-01 04:27:25 +0900

    ✨ Feat: 페이지 이동 훅 useNavagation

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,10 +1,13 @@
 import s from "./Logo.module.scss";
 import LOGO from "@assets/images/main_title.svg";
 import CHAR from "@assets/images/character_full.png";
+import useNavigation from "../../hooks/useNavagation";
 
 const Logo = ({ char = "6rem", width = "31.3125rem" }) => {
+  const { goTo } = useNavigation();
+
   return (
-    <div className={s.logoContainer}>
+    <div className={s.logoContainer} onClick={() => goTo("/")}>
       <img src={CHAR} alt="캐릭터" style={{ width: char }} />
       <img
         src={LOGO}
