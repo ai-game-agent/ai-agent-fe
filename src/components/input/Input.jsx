865f052d39aa4bdd7fbfe036401153dd5fa898a4
Suki <tues1205@naver.com>
2025-12-01 16:25:02 +0900

    ✨ Feat: Input 컴포넌트 props 추가

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,16 +1,31 @@
 import s from "./Input.module.scss";
 import SEARCH from "@assets/icons/search_icon.svg";
 
-const Input = ({ width = "45.1531vw", height = "4.0306vw", onClick }) => {
+const Input = ({
+  width = "45.1531vw",
+  height = "4.0306vw",
+  onClick,
+  value,
+  onChange,
+  onKeyDown,
+  disabled,
+}) => {
   return (
     <div className={s.InputContainer} style={{ width, height }}>
-      <input placeholder="ex. 친구랑 할 수 있는 2만원 이하 협동 공포 게임 추천해줘" />
+      <input
+        placeholder="ex. 친구랑 할 수 있는 2만원 이하 협동 공포 게임 추천해줘"
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        disabled={disabled}
+      />
       <img
         src={SEARCH}
         alt="검색"
         className={s.searchButton}
         style={{ width: "1.3061vw" }}
         onClick={onClick}
+        disabled={disabled}
       />
     </div>
   );
